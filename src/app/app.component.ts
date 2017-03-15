9c1e64b836179d466546a07783f8b93c2a66c1d0
Rick Van <x.rckvan@gmail.com>
2017-03-15 16:47:13 -0600

    custom directives and how to set background color attributes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { HighlightDirective } from './highlight.directive';
+
 @Component({
   selector: 'dir-root',
   templateUrl: './app.component.html',
