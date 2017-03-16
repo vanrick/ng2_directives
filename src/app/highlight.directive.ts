b0dfb23573aa8d73f20cdd9bb296a03da539e7db
Rick Van <x.rckvan@gmail.com>
2017-03-16 16:31:51 -0600

    hovering

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,19 +1,21 @@
-import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
     @HostListener('mouseenter') mouseover(){
-      this.backgroundColor = 'white';
+      this.backgroundColor = this.highlightColor;
     };
     @HostListener('mouseleave') mouseleave(){
-      this.backgroundColor = 'blue';
+      this.backgroundColor = this.defaultColor;
     };
     @HostBinding('style.backgroundColor') get setColor(){
       return this.backgroundColor;
-    }
-    private backgroundColor = 'blue';
+    };
+    @Input() defaultColor = 'white'
+    @Input('highlight') highlightColor = 'blue'
+    private backgroundColor = this.defaultColor;
     constructor() {
       // this.elementRef.nativeElement.style.backgroundColor = "green";
       // this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color','yellow');
